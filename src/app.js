238184b348f6d905eca4d56e4ba1d81241b78466
marcioramires <marcioramiresgt@gmail.com>
2023-02-24 18:27:46 -0300

    vercel configuration

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const sequelize = require("./config/db.js");
 const personRoutes = require("./routes.js");
 const app = express();
 
+const port = process.env.PORT || 3000;
 
 sequelize.sync().then(() => console.log("database connected successfully 🏦"));
 
@@ -10,6 +11,6 @@ app.use(express.json());
 
 app.use("/api/",personRoutes)
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000 🚀");
+app.listen(port, () => {
+  console.log(`Server started on port ${port} 🚀`);
 });
