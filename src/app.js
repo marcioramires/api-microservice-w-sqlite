2f984844329a8ca350d78dc78c6603b229359d9a
marcioramires <marcioramiresgt@gmail.com>
2023-02-23 23:54:11 -0300

    cors updated

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-import { openDb } from './configDB.js';
 import express from 'express'
+import cors from 'cors';
+
+import router from './routes.js'
 
 const app = express();
-app.use(express.json());
 
-openDb();
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
 app.listen(3000, () => console.log('Server running on port 3000 🚀'))
